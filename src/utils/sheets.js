e13b2c73936a729ab67a36ca96073a40ae2b3815
cobiard <jakub.dargaj@vacuumlabs.com>
2020-08-31 11:23:13 +0200

    load sheets with one person only; add error handling to sheets

--- src/utils/sheets.js
+++ src/utils/sheets.js
@@ -12,25 +12,29 @@ const sheetsApi = google.sheets({version: 'v4', auth})
  * Loads data from Google Sheets
  * Returns an array of objects (rows) indexed by column headers (first row)
  * Called from contract template
- * @param {*} people: queried EMS data
+ * @param {*} person: queried EMS data
  * @param {spreadsheetId, sheetId, range, idSheetsColumn, idEMSField} sheet: Google sheet and range identifier, fields to join with EMS
  */
-export const loadSheetData = async (people, sheet) => {
-  const personIDs = people.map((person) => person[sheet.idEMSField])
-
+export const loadSheetData = async (person, sheet) => {
   const [header, ...values] = await getValues(sheet.spreadsheetId, sheet.range)
 
+  if (!header) throw new Error(`Spreadsheet https://docs.google.com/spreadsheets/d/${sheet.spreadsheetId} is empty. Check if sheet ID ${sheet.sheetId} and range ${sheet.range} are correct.`)
+  if (!header.includes(sheet.idSheetsColumn)) throw new Error(`Field ${sheet.idSheetsColumn} not in header. Check the header of spreadsheet https://docs.google.com/spreadsheets/d/${sheet.spreadsheetId} - sheet ID ${sheet.sheetId} - range ${sheet.range}.`)
+  
   // Index by column headers,
-  // filter rows in the array people - from the original request
-  const filteredValues = values
-    .map((row) =>
-      header.reduce((obj, key, col) => {
-        obj[key] = row[col]
-        return obj
-      }, {}),
-    )
-    .filter((row) => personIDs.includes(row[sheet.idSheetsColumn]))
-  return filteredValues
+  // return only the (first) row containing person
+  const indexedRow = values
+  .map((row) =>
+  header.reduce((obj, key, col) => {
+    obj[key] = row[col]
+    return obj
+  }, {}),
+  )
+  .find((row) => row[sheet.idSheetsColumn] === person[sheet.idEMSField])
+
+  if (!indexedRow) throw new Error(`Error joining column ${sheet.idSheetsColumn} of spreadsheet https://docs.google.com/spreadsheets/d/${sheet.spreadsheetId} with column ${sheet.idEMSField} of EMS. Check if ${sheet.idEMSField} of user ID ${person.id} exists in column ${sheet.idSheetsColumn}.`)
+  
+  return indexedRow
 }
 
 const getValues = async (spreadsheetId, range) => {
