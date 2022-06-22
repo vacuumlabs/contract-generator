57d1ff760170d522156624b795fe62b040eed586
Klara Fickova <klara.fickova@vacuumlabs.com>
2022-06-22 19:20:28 +0200

    Upgrade prettier

--- src/utils/retryOnTooManyRequests.js
+++ src/utils/retryOnTooManyRequests.js
@@ -1,14 +1,16 @@
 import {wait} from './wait'
 
-export const retryOnTooManyRequests = (fn) => async (...args) => {
-  try {
-    const res = await fn(...args)
-    return res
-  } catch (res) {
-    if (res.response.status === 429) {
-      await wait(parseInt(res.response.headers['retry-after']) * 1000 + 2000)
-      return fn(...args)
+export const retryOnTooManyRequests =
+  (fn) =>
+  async (...args) => {
+    try {
+      const res = await fn(...args)
+      return res
+    } catch (res) {
+      if (res.response.status === 429) {
+        await wait(parseInt(res.response.headers['retry-after']) * 1000 + 2000)
+        return fn(...args)
+      }
+      throw res
     }
-    throw res
   }
-}
