c019cd531711a05c7543cb0ee4f759c6f93b8217
Teodosie Perisanu <teodosie.perisanu@vacuumlabs.com>
2022-12-19 17:17:34 +0200

    Add basic configuration for server
    Signed-off-by: Teodosie Perisanu <teodosie.perisanu@vacuumlabs.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import express from 'express'
+import helmet from 'helmet'
 import config from './config'
+import cookieParser from 'cookie-parser'
+import helmetOptions from './helmetOptions'
 
 const app = express()
+
+app.set('trust proxy', true)
+
+app.use(cookieParser())
+app.use(helmet(helmetOptions))
+
+app.disable('etag')
 
 const server = app.listen(config.port, () => {
     // eslint-disable-next-line no-console
